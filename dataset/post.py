713129630e38e837576f381e1c5273b29786c4b3
aveygo <gregory.taylor.au@gmail.com>
2025-05-22 22:01:21 +1000

    final

--- dataset/post.py
+++ dataset/post.py
@@ -15,6 +15,33 @@ positive_embs = model.encode([i["positive"] for i in data.values()])
 negative_embs = model.encode([i["negative"] for i in data.values()])
 delta_embs = positive_embs - negative_embs
 
+#delta_embs = model.encode([i["ideal_inspiration"] for i in data.values()])
+
+
+def slerp(v0:np.ndarray, v1:np.ndarray, t:float):
+    DOT_THRESHOLD = 0.9995
+
+    dot = v0.dot(v1)
+    if abs(dot) > DOT_THRESHOLD:
+        v_out =  v0 * (1 - t) + v1 * t
+    else:
+        theta_0 = np.arccos(dot)
+        sin_theta_0 = np.sin(theta_0)
+        theta_t = theta_0 * t
+        sin_theta_t = np.sin(theta_t)
+        s0 = np.sin(theta_0 - theta_t) / sin_theta_0
+        s1 = sin_theta_t / sin_theta_0
+        v_out =  v0 * s0.item() + v1 * s1.item()
+    
+    return v_out
+
+def slerp_average(x:np.ndarray):
+    slerped = [i for i in x]
+    while not len(slerped) == 1:            
+        slerped.append(slerp(slerped[0], slerped[1], 0.5))
+        slerped = slerped[2:]
+    return slerped[-1]
+
 
 party_embs = {}
 
@@ -23,7 +50,7 @@ for idx, (k, v) in enumerate(data.items()):
     party_embs[v["party_src"]].append(delta_embs[idx])
 
 for party in party_embs:
-    party_embs[party] = np.mean(np.array(party_embs[party]), axis=0)
+    party_embs[party] = slerp_average(np.array(party_embs[party]))
 
 party_labels = list(party_embs.keys())
 party_embs = np.array(list(party_embs.values()))
--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import json, numpy as np, base64
 
 with open("emb_data.json", "r") as f:
-    data = json.load(f) # Assumed fetched
+    data = json.load(f)
 
 parties = dict([(k, np.frombuffer(base64.b64decode(v), dtype=np.float32)) for (k, v) in data["parties"].items()])
 policies = dict([(k, np.frombuffer(base64.b64decode(v), dtype=np.float32)) for (k, v) in data["policies"].items()])
 
 def closest(parties, user_emb):
-    """
-    parties == {"party1": numpy embedding1, "party2": numpy embedding2}
-    user_emb == numpy embedding
-    
-    returns list of party names, sorted by cosine distance
-    """
     distances = {party: 1 - np.dot(user_emb, emb) for party, emb in parties.items()}
     return sorted(distances, key=distances.get)
 
@@ -20,11 +14,7 @@ user_emb = np.zeros_like(list(policies.values())[0])
 n = 1
 
 for policy in policies:
-    print(policy)
-    if input("> ") == "y":
-        user_emb += policies[policy]
-    else:
-        user_emb -= policies[policy]
-
+    user_emb += policies[policy] * float(input("-1 to 1 >"))
+    
     n += 1
     print(closest(parties, user_emb / n))
